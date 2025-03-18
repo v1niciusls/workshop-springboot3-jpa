490d124c94d4058756485a67dc64a270d6e70f6a
vinicinls <viniciuslacerda437@gmail.com>
2025-03-18 13:52:40 -0300

    Payment, one-to-one association

--- src/main/java/com/viniciusls/course/entities/Order.java
+++ src/main/java/com/viniciusls/course/entities/Order.java
@@ -35,6 +35,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+    private Payment payment;
     public Order() {
 
     }
@@ -87,7 +89,13 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
 
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 
     @Override
     public boolean equals(Object o) {
