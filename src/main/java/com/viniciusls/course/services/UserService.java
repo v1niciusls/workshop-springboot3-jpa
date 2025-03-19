7cb2bda4c34ea21f806595b2d5eb6939321c5bf4
vinicinls <viniciuslacerda437@gmail.com>
2025-03-18 22:32:27 -0300

    Exception handling - update

--- src/main/java/com/viniciusls/course/services/UserService.java
+++ src/main/java/com/viniciusls/course/services/UserService.java
@@ -4,11 +4,13 @@ import com.viniciusls.course.entities.User;
 import com.viniciusls.course.repositories.UserRepository;
 import com.viniciusls.course.services.exceptions.DatabaseException;
 import com.viniciusls.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
 
     }
 
